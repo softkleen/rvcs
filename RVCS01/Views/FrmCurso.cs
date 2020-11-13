a38bb6747bdae1f7610236f05744da2c77e21536
Wellington Santos <wellington.vsantos@gmail.com>
2020-11-13 09:45:21 -0300

    Preparação de ambiente e criação e implementação de class banco

--- RVCS01/Views/FrmCurso.cs
+++ RVCS01/Views/FrmCurso.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace RVCS01
 {
-    public partial class Form1 : Form
+    public partial class FrmCurso : Form
     {
-        public Form1()
+        public FrmCurso()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
